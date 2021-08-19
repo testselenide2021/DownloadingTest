56a52947c3e3bdfdc0c605762c2c93eaf084f4df
Руслан <testselenide@mail.ru>
2021-08-19 19:58:03 +0500

    Fix to downloading file on Chrome browser

--- src/test/java/DownloadingTest.java
+++ src/test/java/DownloadingTest.java
@@ -8,23 +8,26 @@ import java.util.concurrent.TimeUnit;
 import static com.codeborne.selenide.Configuration.fileDownload;
 import static com.codeborne.selenide.FileDownloadMode.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.FileFilters.withExtension;
+
+
 
 public class DownloadingTest {
     @Test
     void DowloadTest() throws InterruptedException, FileNotFoundException {
         Configuration.timeout=20000;
-        Configuration.browser = "firefox";
+//        Configuration.browser = "firefox";
         Configuration.downloadsFolder = "C:\\TestDownload";
         fileDownload = FOLDER;
-
         open("https://www.google.com/intl/ru_ru/chrome/");
-        File downloadFile = $("button#js-download-hero.chr-cta__button.chr-cta__button--blue." +
-                "js-download.chr-full-bleed-hero__button.chr-cta__button--download." +
-                "js-sticky-trigger.show").shouldBe(Condition.visible).download();
-        TimeUnit.SECONDS.sleep(5);
-        Assertions.assertEquals(downloadFile.getName(), "ChromeSetup.exe");
-    }
 
+        File downloadFile = $("*[id='js-download-hero']").shouldBe(Condition.visible)
+                        .download(withExtension("exe"));
+        TimeUnit.SECONDS.sleep(5);
+        String downloadedFileName = downloadFile.getPath().substring(downloadFile
+                .getPath().lastIndexOf("\\")+1);
+        Assertions.assertEquals("ChromeSetup.exe", downloadedFileName);
+    }
 }
 
 
